4dddee0ee8fda06348aca4fb92b5b3a5607a36d8
Zane Bilous <zanebilous@gmail.com>
2018-03-04 13:23:26 -0800

    Add common Card component

---
+++ src/tpi-boilerplate/components/common/Card.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+import { Stylesheet, Text, View }  from 'react-native'
+
+export default class Card extends Component {
+	render() {
+		return (
+			<View>
+			</View>
+
+		);
+	}
+}
+
+const styles = Stylesheet.create({
+
+
+
+})
+
